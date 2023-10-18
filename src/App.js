e9e4630343ed738941dbbf38b253ee278374403e
aliajaz32 <aliajaz32.aa@gmail.com>
2023-10-18 21:38:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const [todoList,setTodoList] = useState([]);
+  const [inputText,setInputText] = useState('');
+  const [editToggle,setEditToggle] = useState(false);
+  const [editNumber, setEditNumber] = useState()
+  const addTodo = ()=>{
+    console.log('todoList',todoList);
+    console.log('inputText',inputText);
+
+    const tempList = [...todoList];
+    tempList.push(inputText);
+    setTodoList(tempList);
+    setInputText('')
+  
+  }
+  const editTodo =(i)=>{
+    console.log('i',i);
+    setEditNumber(i)
+    setInputText(todoList[i]);
+    setEditToggle(true);    
+  }
+  const updateTodo = ()=>{
+      console.log(editNumber);
+      const tempList = [...todoList]
+      tempList.splice(editNumber,1, inputText);
+      setTodoList(tempList);
+      setEditToggle(false);
+      setInputText('')
+  }
+  const deleteTodo = (i)=>{
+    console.log("delete i ",i)
+    const tempList1 = [...todoList];
+    tempList1.splice(i,1);
+    console.log('tempList1',tempList1);
+    setTodoList(tempList1)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>react todo app</h1>
+      <input type="text" placeholder='enter todo' value={inputText} onChange={(e)=>{ setInputText(  e.target.value)}}    />
+      {editToggle  ? <button onClick={updateTodo}>update</button> :  <button onClick={addTodo}>add Todo</button> } 
+      <ul>
+        {todoList.map(  (item,i)=>{
+          return <li> {item} <button onClick={()=>{ editTodo(i)   }} >edit </button> <button onClick={()=> {deleteTodo(i)}   }  >delete</button>  </li>
+        }   )}
+      </ul>
     </div>
   );
 }
